fe5fc9a917463eade5b067098b39b0a92b08fa62
Idan Asayag <100044797+idanasayag0@users.noreply.github.com>
2023-01-05 08:51:13 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const emailInput = form.elements.email;
 const ageInput = form.elements.age;
 
 
-
 $(window).ready(function() {
     $("#my-form").on("keypress", function (event) {
         var keyPressed = event.keyCode || event.which;
@@ -20,23 +19,75 @@ $(window).ready(function() {
     });
 });
 
-btn.addEventListener("click",()=>{
+// btn.addEventListener("click",()=>{
+//     function createProfile(name, email, age){
+//         if(name.length < 2){
+//             alert("Name must be at least 2 characters long, Profile not created");
+//             form[0].value = "";
+//             return false;
+//         }
+//         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+//         if(!emailRegex.test(email)){
+//             alert("Email is not valid, Profile not created");
+//             form[1].value = "";
+//             return false;
+//         }
+//         const ageRegex = /^[0-9]{1,3}$/;
+//         if((!ageRegex.test(age)) || (age < 0) || (age >= 120)){
+//             alert("Age is not valid, Profile not created");
+//             form[2].value = "";
+//             return false;
+//         }
+//         return {
+//             name,
+//             email,
+//             age
+//         }
+//     }
+//     const profile = createProfile(nameInput.value, emailInput.value, ageInput.value);
+//     if(!profile){
+//         return;
+//     }
+
+//     function addGitHubProfle(userName){
+//         modalText.innerHTML = `<p>Name: ${profile.name}</p><p>Email: ${profile.email}</p><p>Age: ${profile.age}</p>`;
+//         modalText.innerHTML += `<img src="https://github.com/${userName}.png" style="max-width:300px; max-height:300px" alt='Profile picture of ${userName}'>`;
+//     }
+//     modal.style.display="block";
+
+//     const profileImg = addGitHubProfle(nameInput.value);
+//     form.reset();
+// });
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
+
+function createPopUp(){
     function createProfile(name, email, age){
         if(name.length < 2){
             alert("Name must be at least 2 characters long, Profile not created");
-            form.reset();
+            form[0].value = "";
             return false;
         }
         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
         if(!emailRegex.test(email)){
             alert("Email is not valid, Profile not created");
-            form.reset();
+            form[1].value = "";
             return false;
         }
         const ageRegex = /^[0-9]{1,3}$/;
-        if(!ageRegex.test(age)){
+        if((!ageRegex.test(age)) || (age < 0) || (age >= 120)){
             alert("Age is not valid, Profile not created");
-            form.reset();
+            form[2].value = "";
             return false;
         }
         return {
@@ -58,16 +109,14 @@ btn.addEventListener("click",()=>{
 
     const profileImg = addGitHubProfle(nameInput.value);
     form.reset();
-});
-
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+    return {
+        createProfile,
+        addGitHubProfle
     }
-  }
+}
+
+
+
+
+
+
