93cf2d63673c6e043f11e9287e8d0adf8729dd2a
Idan Asayag <100044797+idanasayag0@users.noreply.github.com>
2023-01-05 19:20:32 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -19,47 +19,6 @@ $(window).ready(function() {
     });
 });
 
-// btn.addEventListener("click",()=>{
-//     function createProfile(name, email, age){
-//         if(name.length < 2){
-//             alert("Name must be at least 2 characters long, Profile not created");
-//             form[0].value = "";
-//             return false;
-//         }
-//         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-//         if(!emailRegex.test(email)){
-//             alert("Email is not valid, Profile not created");
-//             form[1].value = "";
-//             return false;
-//         }
-//         const ageRegex = /^[0-9]{1,3}$/;
-//         if((!ageRegex.test(age)) || (age < 0) || (age >= 120)){
-//             alert("Age is not valid, Profile not created");
-//             form[2].value = "";
-//             return false;
-//         }
-//         return {
-//             name,
-//             email,
-//             age
-//         }
-//     }
-//     const profile = createProfile(nameInput.value, emailInput.value, ageInput.value);
-//     if(!profile){
-//         return;
-//     }
-
-//     function addGitHubProfle(userName){
-//         modalText.innerHTML = `<p>Name: ${profile.name}</p><p>Email: ${profile.email}</p><p>Age: ${profile.age}</p>`;
-//         modalText.innerHTML += `<img src="https://github.com/${userName}.png" style="max-width:300px; max-height:300px" alt='Profile picture of ${userName}'>`;
-//     }
-//     modal.style.display="block";
-
-//     const profileImg = addGitHubProfle(nameInput.value);
-//     form.reset();
-// });
-
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
@@ -75,20 +34,32 @@ function createPopUp(){
     function createProfile(name, email, age){
         if(name.length < 2){
             alert("Name must be at least 2 characters long, Profile not created");
-            form[0].value = "";
+            // form[0].value = "";
+            form[0].style.textDecoration = "underline";
+            form[0].style.textDecorationColor = "red";
             return false;
+        }else{
+            form[0].style.textDecoration = "none";
         }
         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
         if(!emailRegex.test(email)){
             alert("Email is not valid, Profile not created");
-            form[1].value = "";
+            // form[1].value = "";
+            form[1].style.textDecoration = "underline";
+            form[1].style.textDecorationColor = "red";
             return false;
+        }else{
+            form[1].style.textDecoration = "none";
         }
         const ageRegex = /^[0-9]{1,3}$/;
         if((!ageRegex.test(age)) || (age < 0) || (age >= 120)){
             alert("Age is not valid, Profile not created");
-            form[2].value = "";
+            // form[2].value = "";
+            form[2].style.textDecoration = "underline";
+            form[2].style.textDecorationColor = "red";
             return false;
+        }else{
+            form[2].style.textDecoration = "none";
         }
         return {
             name,
@@ -115,7 +86,7 @@ function createPopUp(){
     }
 }
 
-
+const test = createPopUp;
 
 
 
